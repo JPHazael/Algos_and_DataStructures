2524ede7831f54319f684075d57660e53db5066c
JPHazael <jeffdaines@gmail.com>
2017-03-13 21:19:53 -0400

    Updated retain cycles and notes.

--- RetainCycle.playground/Contents.swift
+++ RetainCycle.playground/Contents.swift
@@ -4,6 +4,7 @@ import UIKit
 
 class Person{
     var name: String
+    var apartment: Apartment?
     init(name: String) {
         self.name = name
     }
@@ -16,10 +17,13 @@ class Person{
 class Apartment{
     let number: Int
     
-    //set tenant to weak to avoid memory leaks and retention cycles
+    //set tenant to weak to avoid memory leaks and retention cycles. Weak declares a variable without increasing the reference count for the property.
     var tenant: Person?
     init(number:Int) {
         self.number = number
+    }
+    deinit {
+        print("Apartment is being deinitialized")
     }
 }
 
@@ -27,11 +31,15 @@ class Apartment{
 var bob:Person? = Person(name: "Bob")
 let me = Person(name: "Jeff")
 
-let apt = Apartment(number: 123)
+var apt: Apartment? = Apartment(number: 123)
 
-apt.tenant = bob
+apt?.tenant = bob
+// When we create a reference to apt from bob we create a retain cycle because niether class can deinit properly because they are holding on to strong references for each other.
+bob?.apartment = apt
 
 bob = nil
+
+apt = nil
 
 
 //in order to prevent memory leaks you have to deint all strong connections together
